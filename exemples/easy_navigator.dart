55448e7e8c602279bab156908b76c4764a19020a
lucas <lucas.cardo123@hotmail.com>
2023-05-06 15:33:05 -0400

    add example easy navigator

---
+++ exemples/easy_navigator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:manga_easy_routes/manga_easy_routes.dart';
+
+class EasyNavigatorPage extends StatelessWidget {
+  final String idEmblema;
+  const EasyNavigatorPage({super.key, required this.idEmblema});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => EasyNavigator.of(context).pushNamed(
+        route: EasyRoutes.introduce,
+        arguments: {},
+      ),
+      child: Text('test'),
+    );
+  }
+}
