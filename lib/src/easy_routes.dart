80c81d6f9364328aa95bbea768820c18cd5efd26
lucas <lucas.cardo123@hotmail.com>
2023-05-09 19:48:42 -0400

    Add arguments in route /manga

--- lib/src/easy_routes.dart
+++ lib/src/easy_routes.dart
@@ -1,4 +1,5 @@
 import 'package:manga_easy_routes/src/argument_type.dart';
+import 'package:manga_easy_sdk/manga_easy_sdk.dart';
 
 enum EasyRoutes {
   preferences('/Preference'),
@@ -8,7 +9,10 @@ enum EasyRoutes {
     ArgumentType(key: 'idEmblema', type: String),
     ArgumentType(key: 'test', type: double, isRequired: false)
   ]),
-  manga('/manga');
+  manga('/manga', [
+    ArgumentType(key: 'manga', type: Manga),
+    ArgumentType(key: 'detalhes_manga', type: DetalhesManga, isRequired: false)
+  ]);
 
   final String nameRoute;
   final List<ArgumentType>? arguments;
