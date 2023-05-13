cedc28d060b72746578eb9e8afbf208da0f85110
lucas <lucas.cardo123@hotmail.com>
2023-05-13 10:20:03 -0400

    Add route recommendations

--- lib/src/easy_routes.dart
+++ lib/src/easy_routes.dart
@@ -4,6 +4,7 @@ import 'package:manga_easy_sdk/manga_easy_sdk.dart';
 enum EasyRoutes {
   preferences('/Preference'),
   introduce('/introduce'),
+  recommendations('/recommendation-screen'),
   mangaLis('/migrate', [
     ArgumentType(key: 'nameManga', type: String),
     ArgumentType(key: 'isUniqueId', type: bool),
