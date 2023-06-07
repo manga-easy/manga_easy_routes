ca7c68ca288e0dc7f6ca8fe7b87a8ae03f436245
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-06-06 23:43:17 -0300

    remove sdk package and manga route

--- lib/src/easy_routes.dart
+++ lib/src/easy_routes.dart
@@ -1,5 +1,4 @@
 import 'package:manga_easy_routes/src/argument_type.dart';
-import 'package:manga_easy_sdk/manga_easy_sdk.dart';
 
 enum EasyRoutes {
   preferences('/Preference'),
@@ -14,10 +13,6 @@ enum EasyRoutes {
   test('/test', [
     ArgumentType(key: 'idEmblema', type: String),
     ArgumentType(key: 'test', type: double, isRequired: false)
-  ]),
-  manga('/manga', [
-    ArgumentType(key: 'manga', type: Manga),
-    ArgumentType(key: 'detalhes_manga', type: DetalhesManga, isRequired: false)
   ]);
 
   final String nameRoute;
